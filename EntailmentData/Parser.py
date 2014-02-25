32d288a856f5cd66f26c528019f081e8712daabf
xiao25 <tzhang46@illinois.edu>
2014-02-25 11:36:11 -0600

    update Parser.py and create three related datafiles

--- EntailmentData/Parser.py
+++ EntailmentData/Parser.py
@@ -1,30 +1,28 @@
 __author__ = 'ztx'
 import os
-import random
 import sys
-import math
+
 
 def getdata(filename):
     if(os.path.exists(filename)):
             data=open(filename,"r")
-    return data
+            return data
+    else:
+        return False
 
 
-def parse(data):
+def parse(data,type0,type1_1,type1_2):
     data.next()
     result_lst = list()
     for instance in data:
-	print instance
+
         parts=instance.split("\t")
-	print parts        
-		
-	type =parts[4].replace("\n","")
-        if(type=="ENTAILMENT" or type=="CONTRADICTION"):
+        type =parts[4].replace("\n","")
+        if(type==type1_1 or type==type1_2):
             type=1
-        elif(type=="NEUTRAL"):
+        elif(type==type0):
             type=0
         else:
-	    print "type = " + type +". compare with NEUTRAL"
             raise EOFError
         result_lst.append(type)
         result_lst.append(parts[1])
@@ -32,15 +30,32 @@ def parse(data):
     return result_lst
 
 def writetofile(res_lst,res_file):
-    for term in result_lst:
-        result_file.write("%s\n" % term)
+    for term in res_lst:
+        res_file.write("%s\n" % term)
 
+def checktype(type):
+    if(type=="NEUTRAL" or type=="ENTAILMENT" or type=="CONTRADICTION"):
+        return True
+    else:
+        return False
 
+def main():
+    """
+   running command has format:python Parser.py filename type0 type1_1 type1_2; type options are different and from NEUTRAL,ENTAILMENT,CONTRADICTION")
+    """
+    train_file = sys.argv[1]
+    type0=sys.argv[2]
+    type1_1=sys.argv[3]
+    type1_2 = sys.argv[4]
+    if(checktype(type0) and checktype(type1_1) and checktype(type1_2)):
+        train_data = getdata(train_file)
+        result_lst = list()
+        if(train_data):
+            result_lst=parse(train_data,type0,type1_1,type1_2)
+        result_file = open(type0+"VsOthers.txt", "w")
+        writetofile(result_lst,result_file)
+    else:
+       print("three different option types can only be NEUTRAL,ENTAILMENT,CONTRADICTION!")
 
-train_file = sys.argv[1]
-train_data = getdata(train_file)
-result_lst = list()
-if(train_data):
-    result_lst=parse(train_data)
-result_file = open("input.txt", "w")
-writetofile(result_lst,result_file)
+if __name__ == "__main__":
+    main()
