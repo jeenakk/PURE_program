ab3f2c624ffbba1847790acfb1aa6dad97bb9540
Danyal Khashabi <khashab2@silverbridge.cs.illinois.edu>
2014-04-20 01:49:08 -0500

    adding stanford parser

---
+++ RecursiveLanguageModel/Code/writeOutTheSentences.m
@@ -0,0 +1,12 @@
+clear all; close all; clc; 
+
+files = '../Data/'; 
+data = textread( [files '100data.txt' ] , '%s', 'delimiter', '\n'); 
+definitions = data(2:3:end);  
+
+f = fopen('../Data/onlySentences.txt', 'w'); 
+for i = 1:size(definitions,1)
+    fprintf(f, [ definitions{i}  '\n'])
+    fprintf(f, '############\n' )
+end 
+
